71c6637d4422edb7d4df49f23138d404506c2257
JasonWilger <jkw500@gmail.com>
2021-03-02 22:21:48 -0500

    connecting to elephant

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "http://drona.db.elephantsql.com",
   headers: {
     "Content-type": "application/json"
   }
